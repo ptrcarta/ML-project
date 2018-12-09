c686be559266cd66fbf847bd4055271ceb1b45ad
Jonathan Besomi <43236409+JonathanBesomi@users.noreply.github.com>
2018-12-09 11:22:36 +0100

    Simple Keras CIFAR model

--- models/cifar10/cifar10_model.py
+++ models/cifar10/cifar10_model.py
@@ -2,29 +2,16 @@ from models.AbstractModel import AbstractModel
 
 import tensorflow as tf
 
-import models.cifar10.cifar10_training as cifar10_training
-import models.cifar10.cifar10_evaluating as cifar10_evaluating
-import models.cifar10.cifar10_nn as cifar10_nn
+import models.cifar10.cifar10_train as cifar10_train
 
-# Disclaimer: code adapted from 'Advanced Convolutional Neural Networks' (TF tutorial)
-# https://www.tensorflow.org/tutorials/images/deep_cnn
+from time import time
+
 
 class cifar10_model(AbstractModel):
 
-    def __download_and_check_dir(self, path):
-        # If not already done, download and extract locally the dataset
-        cifar10_nn.maybe_download_and_extract()
-
-        # Make directory to write events logs and checkpoints
-        if tf.gfile.Exists(path):
-            tf.gfile.DeleteRecursively(path)
-        tf.gfile.MakeDirs(path)
-
     def train(self, path):
-        """Train the naive model (without fingerprints)"""
-        self.__download_and_check_dir(path)
-        cifar10_training.train(path)
+        pass
+
 
     def evaluate(self):
-        """Evaluate the naive trained model (without fingerprints)"""
-        cifar10_evaluating.evaluate()
+        pass
